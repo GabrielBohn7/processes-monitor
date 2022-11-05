32945f6f3256213b90008ed1e31902cbf3dd71ac
GabrielBohn7 <gabriel.bohn1@gmail.com>
2022-11-05 07:41:26 -0300

    tests separation into Expeceted and Invalid

--- TestMonitor/MonitorProgramsTests.cs
+++ TestMonitor/MonitorProgramsTests.cs
@@ -1,30 +1,181 @@
 using System.Diagnostics;
 
-namespace TestMonitor
+namespace TestMonitor.MonitorProgramsTests
 {
-    public class Tests
+    public class MonitorProgramsExpectedKilllogTests
     {
         [Test]
-        public void MonitorPrograms_With0NumericValues_ReturnInvalidFormat()
+        public void MonitorPrograms_With3processToMonitor_ReturnExpectedKillLog()
         {
             //Arrange
-            const string COMMAND_ONE = "a a a";
+            const string PROCESS_ONE = "notepad.exe";
+            const string PROCESS_TWO = "wscript.exe";
+            const string PROCESS_THREE = "mspaint.exe";
 
-            const string EXPECTED_LOGL_ONE = "Invalid Command Format";
+            const string COMMAND_ONE = "notepad 2 1";
+            const string COMMAND_TWO = "wscript 2 1";
+            const string COMMAND_THREE = "mspaint 2 1";
+
+            const string EXPECTED_LOGL_ONE = "Process notepad did not exceed threshold";
+            const string EXPECTED_LOGL_TWO = "Process wscript did not exceed threshold";
+            const string EXPECTED_LOGL_THREE = "Process mspaint did not exceed threshold";
+            const string EXPECTED_LOGL_FOUR = "Process notepad killed";
+            const string EXPECTED_LOGL_FIVE = "Process wscript killed";
+            const string EXPECTED_LOGL_SIX = "Process mspaint killed";
+
+            var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>()
+            {
+                new Tuple<string[], Stopwatch>(COMMAND_ONE.Split(' '), new Stopwatch()),
+                new Tuple<string[], Stopwatch>(COMMAND_TWO.Split(' '), new Stopwatch()),
+                new Tuple<string[], Stopwatch>(COMMAND_THREE.Split(' '), new Stopwatch())
+            };
+
+            Process.Start(PROCESS_ONE);
+            Process.Start(PROCESS_TWO);
+            Process.Start(PROCESS_THREE);
+
+            //Expected
+            List<string> expectedKillLog = new List<string>();
+            expectedKillLog.Add(EXPECTED_LOGL_ONE);
+            expectedKillLog.Add(EXPECTED_LOGL_TWO);
+            expectedKillLog.Add(EXPECTED_LOGL_THREE);
+            expectedKillLog.Add(EXPECTED_LOGL_FOUR);
+            expectedKillLog.Add(EXPECTED_LOGL_FIVE);
+            expectedKillLog.Add(EXPECTED_LOGL_SIX);
+
+            //Act
+            MonitorLocalMachineService.StartTimers(processToMonitorTuple);
+
+            //var killLog = MonitorLocalMachineService.MonitorWindowsProcesses(processToMonitorTuple);
+            List<string> killLogAsync = new List<string>();
+            while (processToMonitorTuple.Count > 0)
+            {
+                Thread.Sleep(1000);
+                killLogAsync.AddRange(MonitorLocalMachineService.MonitorWindowsProcessesAsync(processToMonitorTuple));
+            }
+
+            //Assert
+            Assert.That(killLogAsync, Is.EqualTo(expectedKillLog));
+        }
+
+        [Test]
+        public void MonitorPrograms_With2equalProcessToMonitor_ReturnExpectedKillLog()
+        {
+            //Arrange
+            const string PROCESS_ONE = "notepad.exe";
+
+            const string COMMAND_ONE = "notepad 2 1";
+            const string COMMAND_TWO = "notepad 2 1";
+
+            const string EXPECTED_LOGL_ONE = "Process notepad did not exceed threshold";
+            const string EXPECTED_LOGL_TWO = "Process notepad did not exceed threshold";
+            const string EXPECTED_LOGL_THREE = "Process notepad killed";
+            const string EXPECTED_LOGL_FOUR = "Process notepad killed";
+
+            var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>()
+            {
+                new Tuple<string[], Stopwatch>(COMMAND_ONE.Split(' '), new Stopwatch()),
+                new Tuple<string[], Stopwatch>(COMMAND_TWO.Split(' '), new Stopwatch())
+            };
+
+            Process.Start(PROCESS_ONE);
+
+            //Expected
+            List<string> expectedKillLog = new List<string>();
+            expectedKillLog.Add(EXPECTED_LOGL_ONE);
+            expectedKillLog.Add(EXPECTED_LOGL_TWO);
+            expectedKillLog.Add(EXPECTED_LOGL_THREE);
+            expectedKillLog.Add(EXPECTED_LOGL_FOUR);
+
+            //Act
+            MonitorLocalMachineService.StartTimers(processToMonitorTuple);
+
+            //var killLog = MonitorLocalMachineService.MonitorWindowsProcesses(processToMonitorTuple);
+
+            //TEST ASYNC METHOD: SAME EQUALLY USER IN PROGRAM.CS
+            List<string> killLogAsync = new List<string>();
+            while (processToMonitorTuple.Count > 0)
+            {
+                Thread.Sleep(1000);
+                killLogAsync.AddRange(MonitorLocalMachineService.MonitorWindowsProcessesAsync(processToMonitorTuple));
+            }
+
+            //Assert
+            Assert.That(killLogAsync, Is.EqualTo(expectedKillLog));
+        }
+
+        [Test]
+        public void MonitorPrograms_With1processToMonitor_ReturnExpectedKillLog()
+        {
+            //Arrange
+            const string PROCESS_ONE = "notepad.exe";
+
+            const string COMMAND_ONE = "notepad 2 1";
+
+            const string EXPECTED_LOGL_ONE = "Process notepad did not exceed threshold";
+            const string EXPECTED_LOGL_TWO = "Process notepad killed";
 
             var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>()
             {
                 new Tuple<string[], Stopwatch>(COMMAND_ONE.Split(' '), new Stopwatch())
             };
 
+            Process.Start(PROCESS_ONE);
+
             //Expected
             List<string> expectedKillLog = new List<string>();
             expectedKillLog.Add(EXPECTED_LOGL_ONE);
+            expectedKillLog.Add(EXPECTED_LOGL_TWO);
 
             //Act
             MonitorLocalMachineService.StartTimers(processToMonitorTuple);
 
-            //TEST SYNC METHOD: For less logic on tests.cs files. This method would not require the while loop down below
+            //var killLog = MonitorLocalMachineService.MonitorWindowsProcesses(processToMonitorTuple);
+            List<string> killLogAsync = new List<string>();
+            while (processToMonitorTuple.Count > 0)
+            {
+                Thread.Sleep(1000);
+                killLogAsync.AddRange(MonitorLocalMachineService.MonitorWindowsProcessesAsync(processToMonitorTuple));
+            }
+
+            //Assert
+            Assert.That(killLogAsync, Is.EqualTo(expectedKillLog));
+        }
+
+        [Test]
+        public void MonitorPrograms_With2processToMonitor_ReturnExpectedKillLog()
+        {
+            //Arrange
+            const string PROCESS_ONE = "notepad.exe";
+            const string PROCESS_TWO = "wscript.exe";
+
+            const string COMMAND_ONE = "notepad 2 1";
+            const string COMMAND_TWO = "wscript 2 1";
+
+            const string EXPECTED_LOGL_ONE = "Process notepad did not exceed threshold";
+            const string EXPECTED_LOGL_TWO = "Process wscript did not exceed threshold";
+            const string EXPECTED_LOGL_THREE = "Process notepad killed";
+            const string EXPECTED_LOGL_FOUR = "Process wscript killed";
+
+            var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>()
+            {
+                new Tuple<string[], Stopwatch>(COMMAND_ONE.Split(' '), new Stopwatch()),
+                new Tuple<string[], Stopwatch>(COMMAND_TWO.Split(' '), new Stopwatch())
+            };
+
+            Process.Start(PROCESS_ONE);
+            Process.Start(PROCESS_TWO);
+
+            //Expected
+            List<string> expectedKillLog = new List<string>();
+            expectedKillLog.Add(EXPECTED_LOGL_ONE);
+            expectedKillLog.Add(EXPECTED_LOGL_TWO);
+            expectedKillLog.Add(EXPECTED_LOGL_THREE);
+            expectedKillLog.Add(EXPECTED_LOGL_FOUR);
+
+            //Act
+            MonitorLocalMachineService.StartTimers(processToMonitorTuple);
+
             //var killLog = MonitorLocalMachineService.MonitorWindowsProcesses(processToMonitorTuple);
             List<string> killLogAsync = new List<string>();
             while (processToMonitorTuple.Count > 0)
@@ -140,6 +291,46 @@ namespace TestMonitor
             //Assert
             Assert.That(killLogAsync, Is.EqualTo(expectedKillLog));
         }
+
+
+    }
+
+    public class MonitorProgramsInvalidFormatTests
+    {
+        [Test]
+        public void MonitorPrograms_With0NumericValues_ReturnInvalidFormat()
+        {
+            //Arrange
+            const string COMMAND_ONE = "a a a";
+
+            const string EXPECTED_LOGL_ONE = "Invalid Command Format";
+
+            var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>()
+            {
+                new Tuple<string[], Stopwatch>(COMMAND_ONE.Split(' '), new Stopwatch())
+            };
+
+            //Expected
+            List<string> expectedKillLog = new List<string>();
+            expectedKillLog.Add(EXPECTED_LOGL_ONE);
+
+            //Act
+            MonitorLocalMachineService.StartTimers(processToMonitorTuple);
+
+            //TEST SYNC METHOD: For less logic on tests.cs files. This method would not require the while loop down below
+            //var killLog = MonitorLocalMachineService.MonitorWindowsProcesses(processToMonitorTuple);
+            List<string> killLogAsync = new List<string>();
+            while (processToMonitorTuple.Count > 0)
+            {
+                Thread.Sleep(1000);
+                killLogAsync.AddRange(MonitorLocalMachineService.MonitorWindowsProcessesAsync(processToMonitorTuple));
+            }
+
+            //Assert
+            Assert.That(killLogAsync, Is.EqualTo(expectedKillLog));
+        }
+
+
 
         [Test]
         public void MonitorPrograms_WithNonAlphanumericArgumets_ReturnInvalidFormat()
@@ -320,190 +511,6 @@ namespace TestMonitor
             //Assert
             Assert.That(killLogAsync, Is.EqualTo(expectedKillLog));
         }
-
-        [Test]
-        public void MonitorPrograms_With2equalProcessToMonitor_ReturnExpectedKillLog()
-        {
-            //Arrange
-            const string PROCESS_ONE = "notepad.exe";
-
-            const string COMMAND_ONE = "notepad 2 1";
-            const string COMMAND_TWO = "notepad 2 1";
-
-            const string EXPECTED_LOGL_ONE = "Process notepad did not exceed threshold";
-            const string EXPECTED_LOGL_TWO = "Process notepad did not exceed threshold";
-            const string EXPECTED_LOGL_THREE = "Process notepad killed";
-            const string EXPECTED_LOGL_FOUR = "Process notepad is not running";
-
-            var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>()
-            {
-                new Tuple<string[], Stopwatch>(COMMAND_ONE.Split(' '), new Stopwatch()),
-                new Tuple<string[], Stopwatch>(COMMAND_TWO.Split(' '), new Stopwatch())
-            };
-
-            Process.Start(PROCESS_ONE);
-
-            //Expected
-            List<string> expectedKillLog = new List<string>();
-            expectedKillLog.Add(EXPECTED_LOGL_ONE);
-            expectedKillLog.Add(EXPECTED_LOGL_TWO);
-            expectedKillLog.Add(EXPECTED_LOGL_THREE);
-            expectedKillLog.Add(EXPECTED_LOGL_FOUR);
-
-            //Act
-            MonitorLocalMachineService.StartTimers(processToMonitorTuple);
-
-            //var killLog = MonitorLocalMachineService.MonitorWindowsProcesses(processToMonitorTuple);
-
-            //TEST ASYNC METHOD: SAME EQUALLY USER IN PROGRAM.CS
-            List<string> killLogAsync = new List<string>();
-            while (processToMonitorTuple.Count > 0)
-            {
-                Thread.Sleep(1000);
-                killLogAsync.AddRange(MonitorLocalMachineService.MonitorWindowsProcessesAsync(processToMonitorTuple));
-            }
-
-            //Assert
-            Assert.That(killLogAsync, Is.EqualTo(expectedKillLog));
-        }
-
-        [Test]
-        public void MonitorPrograms_With1processToMonitor_ReturnExpectedKillLog()
-        {
-            //Arrange
-            const string PROCESS_ONE = "notepad.exe";
-
-            const string COMMAND_ONE = "notepad 2 1";
-
-            const string EXPECTED_LOGL_ONE = "Process notepad did not exceed threshold";
-            const string EXPECTED_LOGL_TWO = "Process notepad killed";
-
-            var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>()
-            {
-                new Tuple<string[], Stopwatch>(COMMAND_ONE.Split(' '), new Stopwatch())
-            };
-
-            Process.Start(PROCESS_ONE);
-
-            //Expected
-            List<string> expectedKillLog = new List<string>();
-            expectedKillLog.Add(EXPECTED_LOGL_ONE);
-            expectedKillLog.Add(EXPECTED_LOGL_TWO);
-
-            //Act
-            MonitorLocalMachineService.StartTimers(processToMonitorTuple);
-
-            //var killLog = MonitorLocalMachineService.MonitorWindowsProcesses(processToMonitorTuple);
-            List<string> killLogAsync = new List<string>();
-            while (processToMonitorTuple.Count > 0)
-            {
-                Thread.Sleep(1000);
-                killLogAsync.AddRange(MonitorLocalMachineService.MonitorWindowsProcessesAsync(processToMonitorTuple));
-            }
-
-            //Assert
-            Assert.That(killLogAsync, Is.EqualTo(expectedKillLog));
-        }
-
-        [Test]
-        public void MonitorPrograms_With2processToMonitor_ReturnExpectedKillLog()
-        {
-            //Arrange
-            const string PROCESS_ONE = "notepad.exe";
-            const string PROCESS_TWO = "wscript.exe";
-
-            const string COMMAND_ONE = "notepad 2 1";
-            const string COMMAND_TWO = "wscript 2 1";
-
-            const string EXPECTED_LOGL_ONE   = "Process notepad did not exceed threshold";
-            const string EXPECTED_LOGL_TWO   = "Process wscript did not exceed threshold";
-            const string EXPECTED_LOGL_THREE = "Process notepad killed";
-            const string EXPECTED_LOGL_FOUR  = "Process wscript killed";
-
-            var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>()
-            {
-                new Tuple<string[], Stopwatch>(COMMAND_ONE.Split(' '), new Stopwatch()),
-                new Tuple<string[], Stopwatch>(COMMAND_TWO.Split(' '), new Stopwatch())
-            };
-
-            Process.Start(PROCESS_ONE);
-            Process.Start(PROCESS_TWO);
-
-            //Expected
-            List<string> expectedKillLog = new List<string>();
-            expectedKillLog.Add(EXPECTED_LOGL_ONE);
-            expectedKillLog.Add(EXPECTED_LOGL_TWO);
-            expectedKillLog.Add(EXPECTED_LOGL_THREE);
-            expectedKillLog.Add(EXPECTED_LOGL_FOUR);
-
-            //Act
-            MonitorLocalMachineService.StartTimers(processToMonitorTuple);
-
-            //var killLog = MonitorLocalMachineService.MonitorWindowsProcesses(processToMonitorTuple);
-            List<string> killLogAsync = new List<string>();
-            while (processToMonitorTuple.Count > 0)
-            {
-                Thread.Sleep(1000);
-                killLogAsync.AddRange(MonitorLocalMachineService.MonitorWindowsProcessesAsync(processToMonitorTuple));
-            }
-
-            //Assert
-            Assert.That(killLogAsync, Is.EqualTo(expectedKillLog));
-        }
-
-        [Test]
-        public void MonitorPrograms_With3processToMonitor_ReturnExpectedKillLog()
-        {
-            //Arrange
-            const string PROCESS_ONE = "notepad.exe";
-            const string PROCESS_TWO = "wscript.exe";
-            const string PROCESS_THREE = "mspaint.exe";
-
-            const string COMMAND_ONE = "notepad 2 1";
-            const string COMMAND_TWO = "wscript 2 1";
-            const string COMMAND_THREE = "mspaint 2 1";
-
-            const string EXPECTED_LOGL_ONE = "Process notepad did not exceed threshold";
-            const string EXPECTED_LOGL_TWO = "Process wscript did not exceed threshold";
-            const string EXPECTED_LOGL_THREE = "Process mspaint did not exceed threshold";
-            const string EXPECTED_LOGL_FOUR = "Process notepad killed";
-            const string EXPECTED_LOGL_FIVE = "Process wscript killed";
-            const string EXPECTED_LOGL_SIX = "Process mspaint killed";
-
-            var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>()
-            {
-                new Tuple<string[], Stopwatch>(COMMAND_ONE.Split(' '), new Stopwatch()),
-                new Tuple<string[], Stopwatch>(COMMAND_TWO.Split(' '), new Stopwatch()),
-                new Tuple<string[], Stopwatch>(COMMAND_THREE.Split(' '), new Stopwatch())
-            };
-
-            Process.Start(PROCESS_ONE);
-            Process.Start(PROCESS_TWO);
-            Process.Start(PROCESS_THREE);
-
-            //Expected
-            List<string> expectedKillLog = new List<string>();
-            expectedKillLog.Add(EXPECTED_LOGL_ONE);
-            expectedKillLog.Add(EXPECTED_LOGL_TWO);
-            expectedKillLog.Add(EXPECTED_LOGL_THREE);
-            expectedKillLog.Add(EXPECTED_LOGL_FOUR);
-            expectedKillLog.Add(EXPECTED_LOGL_FIVE);
-            expectedKillLog.Add(EXPECTED_LOGL_SIX);
-
-            //Act
-            MonitorLocalMachineService.StartTimers(processToMonitorTuple);
-
-            //var killLog = MonitorLocalMachineService.MonitorWindowsProcesses(processToMonitorTuple);
-            List<string> killLogAsync = new List<string>();
-            while (processToMonitorTuple.Count > 0)
-            {
-                Thread.Sleep(1000);
-                killLogAsync.AddRange(MonitorLocalMachineService.MonitorWindowsProcessesAsync(processToMonitorTuple));
-            }
-
-            //Assert
-            Assert.That(killLogAsync, Is.EqualTo(expectedKillLog));
-        }
-
     }
 }
+    
---
+++ TestMonitor/StartStopwatchTests.cs
@@ -0,0 +1,60 @@
+using System.Diagnostics;
+
+namespace TestMonitor.StartStartTimers
+{
+    public class StartStartTimers
+    {
+        [Test]
+        public void StartTimers_With0Commands_ReturnInvalid()
+        {
+            //Arrange
+            var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>();
+
+            //Act
+            MonitorLocalMachineService.StartTimers(processToMonitorTuple);
+
+            //Assert
+            Assert.IsEmpty(processToMonitorTuple);
+        }
+
+        [Test]
+        public void StartTimers_With1Command_ReturnTrue()
+        {
+            //Arrange
+            const string COMMAND_ONE = "notepad 2 1";
+
+            var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>()
+            {
+                new Tuple<string[], Stopwatch>(COMMAND_ONE.Split(' '), new Stopwatch())
+            };
+
+            //Act
+            MonitorLocalMachineService.StartTimers(processToMonitorTuple);
+
+            //Assert
+            Assert.That(processToMonitorTuple[0].Item2.Elapsed.TotalSeconds > 0, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void StartTimers_With2Commands_ReturnTrue()
+        {
+            //Arrange
+            const string COMMAND_ONE = "notepad 2 1";
+            const string COMMAND_TWO = "wscript 2 1";
+
+            var processToMonitorTuple = new List<Tuple<string[], Stopwatch>>()
+            {
+                new Tuple<string[], Stopwatch>(COMMAND_ONE.Split(' '), new Stopwatch()),
+                new Tuple<string[], Stopwatch>(COMMAND_TWO.Split(' '), new Stopwatch())
+            };
+
+            //Act
+            MonitorLocalMachineService.StartTimers(processToMonitorTuple);
+
+            //Assert
+            Assert.That(processToMonitorTuple[0].Item2.Elapsed.TotalSeconds > 0, Is.EqualTo(true));
+            Assert.That(processToMonitorTuple[1].Item2.Elapsed.TotalSeconds > 0, Is.EqualTo(true));
+        }
+
+    }
+}
